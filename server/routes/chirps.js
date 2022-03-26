e4301286b66c07942aedae224b1180c024da867e
M-Rozell <95882154+M-Rozell@users.noreply.github.com>
2022-03-25 19:20:54 -0500

    first part completed

---
+++ server/routes/chirps.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const chirpsStore = require('../../chirpstore');
+let router = express.Router();
+
+router.get('/:id?', (req, res) => {
+    //chirpsStore.CreateChirp(req.body)
+    let id = req.params.id
+    if (id) {
+        res.json(chirpsStore.GetChirp(id))
+    } else {
+        res.send(chirpsStore.GetChirps())
+    }
+    res.sendStatus(200)
+})
+
+router.post('/', (req, res) => {
+    chirpsStore.CreateChirp(req.body);
+    res.sendStatus(200)
+})
+
+router.put('/.id', (req, res) => {
+    chirpsStore.UpdateChirp(req.params.id, req.body)
+    res.sendStatus(200)
+})
+
+router.delete('/:id', (req, res) => {
+    chirpsStore.DeleteChirp(req.params.id)
+    res.sendStatus(200);
+})
+
+module.exports = router;
